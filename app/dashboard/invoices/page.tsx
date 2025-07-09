d7480ab1a0b4327ea92ead74e29cd17c23641e76
Soje <114420938+BerllyDislink@users.noreply.github.com>
2025-07-09 12:29:43 -0500

    Actualización de archivo package.json next:latest, además de actualización de componente search con URLsearchParams, instanciado con uso de searchParams para párametros de url actual

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -15,8 +15,10 @@ export default async function Page() {
                 </h1>
             </div>
             <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-                <Search placeholder="Search invoices..." />
-                <CreateInvoice />
+            <Suspense fallback={<div>Loading search...</div>}>
+          <Search placeholder="Search invoices..." />
+        </Suspense>
+        <CreateInvoice />
             </div>
             {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
