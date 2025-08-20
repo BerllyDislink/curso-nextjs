081960422d779b19272f18e62dfb2f25dc4edac8
Soje <114420938+BerllyDislink@users.noreply.github.com>
2025-08-20 12:04:40 -0500

    Se crea el archivo not-found para manejar recursos no encontrados, además de hacer pequeños cambios en la función fetchInvoiceById

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,5 +1,6 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { notFound } from 'next/navigation'; //importación de notFound
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 
 export default async function Page(props: {params: Promise<{id: string}>}) { //se obtiene el id de la invoice
@@ -9,6 +10,10 @@ export default async function Page(props: {params: Promise<{id: string}>}) { //s
         fetchInvoiceById(id), //se obtiene la invoice
         fetchCustomers(), //se obtienen los customers
     ]);
+
+    if(!invoice){
+        notFound(); //código agregado de notFound para mostrar el componente en caso de que no exista la factura buscada.
+    }
     return(
         <main>
             <Breadcrumbs
