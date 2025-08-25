cd312abe9f9c019e270f66faa4a8fbbcfbe5b8b3
Soje <114420938+BerllyDislink@users.noreply.github.com>
2025-08-25 17:12:17 -0500

    Se agregan cambios en el archivo actions.ts y create-form.tsx para validar errores en el formulario de creación

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useActionState } from 'react';
 import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
 import {
@@ -9,11 +10,14 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
-import { createInvoice } from '@/app/lib/actions';
+import { createInvoice, State } from '@/app/lib/actions';
+
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  const initialState: State = {message: null, errors: {}};
+  const [state, formAction] = useActionState(createInvoice, initialState);
   return (
-    <form action={createInvoice}> {/*action es el nombre del action que se encuentra en el archivo actions.ts*/}
+    <form action={formAction}> {/*action es el nombre del action (antes era createInvoice) que se encuentra en el archivo actions.ts*/}
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -112,4 +116,5 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
       </div>
     </form>
   );
+  
 }
